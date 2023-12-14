039d441a8d7bf39a91dde59d660715752ac408c0
Cody Adam <cody.ad4@gmail.com>
2023-12-14 15:00:48 +0100

    Add Spark script for CSV analysis

--- python/script.py
+++ python/script.py
@@ -1,7 +1,8 @@
-
-data_file_path = "../data/agg_match_stats_10000.csv"
-
+# %%
 from pyspark.sql import SparkSession
+
+# %%
+data_file_path = "data/agg_match_stats_10000.csv"
 
 # Create a SparkSession
 spark = SparkSession.builder.appName("CSV Analysis").getOrCreate()
@@ -9,6 +10,11 @@ spark = SparkSession.builder.appName("CSV Analysis").getOrCreate()
 # Load the CSV file into a DataFrame
 df = spark.read.csv(data_file_path, header=True, inferSchema=True)
 
+# Show the schema of the DataFrame
+df.printSchema()
+
+
+# %%
 # Show the schema of the DataFrame
 df.printSchema()
 
@@ -22,3 +28,5 @@ print("Number of columns: ", len(df.columns))
 # You can also perform more specific data analysis depending on your needs
 # For example, to find the average of a column named 'column_name', you can do:
 # df.select(avg("column_name")).show()
+
+
